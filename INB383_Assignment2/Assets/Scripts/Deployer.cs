01ba570619af374cf44834d2ad64a609ed863e43
Thanat Chokwijitkul <alexenriquent@gmail.com>
2016-05-18 11:08:55 +1000

    Enhanced fish alignment

--- INB383_Assignment2/Assets/Scripts/Deployer.cs
+++ INB383_Assignment2/Assets/Scripts/Deployer.cs
@@ -11,6 +11,7 @@ public class Deployer : MonoBehaviour {
     private const float minRange = 5.0f;
     private const float maxRange = 15.0f;
     private const float randRange = 10.0f;
+    private const float fishRange = 10.0f;
     private const float sharkRange = 3.0f;
 
 	void Start() {
@@ -20,9 +21,9 @@ public class Deployer : MonoBehaviour {
 
     private void DeployFishBoids() {
         for (int i = 0; i < numFishes; i++) {
-            Instantiate(fishBoid, new Vector3(Random.Range(-randRange, randRange),
-                Random.Range(minRange, maxRange), Random.Range(-randRange, randRange)), 
-                Random.rotation);
+            Instantiate(fishBoid, new Vector3(Random.value * fishRange,
+                Random.value * fishRange + 1.0f, Random.value * fishRange), 
+                Quaternion.identity);
         }
     }
 
